4896452321b37996329fd4d6b33d0d616ce12f2d
Kevin Sage <ksagedeveloper@gmail.com>
2019-09-27 00:40:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return ("Happy holidays, " + name + "!");
 }
-function happyHolidayTo(holdiay, name) {
+function happyHolidayTo(holiday, name) {
   return ("Happy " + holiday + ", " +  name + "!");
 }
